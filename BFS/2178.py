78b9c6d58d34b5c1a95af58fb668cf44c5d0fdee
sm4640 <s1m0611@naver.com>
2024-04-01 11:19:03 +0900

    2178번 성공

--- BFS/2178.py
+++ BFS/2178.py
@@ -1,1 +1,30 @@
 # 미로탐색
+from collections import deque
+
+n, m = map(int, input().split())
+
+maze = []
+for i in range(n):
+    maze.append(input())
+
+check = [[0] * m for _ in range(n)]
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, -1, 1]
+
+q = deque()
+q.append((0,0))
+check[0][0] = 1
+
+while q:
+    a, b = q.popleft()
+    for i in range(4):
+        na = a + dy[i]
+        nb = b + dx[i]
+        if na < 0 or na > n-1 or nb < 0 or nb > m-1:
+            continue
+        if maze[na][nb] == '1' and check[na][nb] == 0:
+            q.append((na,nb))
+            check[na][nb] = check[a][b] + 1
+
+print(check[n-1][m-1])
