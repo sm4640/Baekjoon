30fce9e0d21ef31b42f0915e4d46ffcab013cb7b
sm4640 <s1m0611@naver.com>
2024-09-04 19:52:06 +0900

    9251 또 성공(0904)

--- DP/9251.py
+++ DP/9251.py
@@ -28,3 +28,37 @@ for i in range(1,len(b)):
             dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
 print(dp[len(b)-1][len(a)-1])
+
+
+# LCS (3달 후 또 품 -> 이게 좀 더 빠름)
+
+s1 = input()
+s2 = input()
+
+dp = [[0] * len(s1) for _ in range(len(s2))]
+
+if s1[0] == s2[0]:
+    dp[0][0] = 1
+
+for i in range(1, len(s1)):
+    if dp[0][i-1] == 0:
+        if s2[0] == s1[i]:
+            dp[0][i] = 1
+    else:
+        dp[0][i] = 1
+
+for i in range(1, len(s2)):
+    if dp[i-1][0] == 0:
+        if s1[0] == s2[i]:
+            dp[i][0] = 1
+    else:
+        dp[i][0] = 1
+
+for i in range(1, len(s2)):
+    for j in range(1, len(s1)):
+        if s2[i] == s1[j]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+print(dp[len(s2)-1][len(s1)-1])
