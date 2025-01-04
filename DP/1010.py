61d423dd8a81bb31b4a5195115a9f2ce12f44148
sm4640 <s1m0611@naver.com>
2025-01-04 16:33:05 +0900

    1010 두번째 성공

--- DP/1010.py
+++ DP/1010.py
@@ -2,12 +2,62 @@
 
 t = int(input())
 
-for _ in range(t):
+for i in range(t):
     n,m = map(int, input().split())
-    dividend = 1
-    for i in range(n):
-        dividend *= m-i
-    divisor = 1
-    for i in range(n,0,-1):
-        divisor *= i
-    print(dividend // divisor)
+    son, mom = 1, 1
+    for j in range(m, m-n, -1):
+        print("j: ", j)
+        son *= j
+    for k in range(n, 0, -1):
+        print("k: ", k)
+        mom *= k
+    print(son//mom)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# t = int(input())
+
+# for _ in range(t):
+#     n,m = map(int, input().split())
+#     dividend = 1
+#     for i in range(n):
+#         dividend *= m-i
+#     divisor = 1
+#     for i in range(n,0,-1):
+#         divisor *= i
+#     print(dividend // divisor)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
